b357057c9c3fa75b5e60c4e398dc143c0ab73387
Bader Kharbat <unconcernedproductions@gmail.com>
2019-05-14 18:57:35 -0400

    Added Cayenne Files, BaseDataObject not being imported?

---
+++ vaadinplay/src/main/java/CayenneTesting.java
@@ -0,0 +1,9 @@
+
+public class CayenneTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ vaadinplay/src/main/java/cayennemodel/Users.java
@@ -0,0 +1,9 @@
+package cayennemodel;
+
+import cayennemodel.auto._Users;
+
+public class Users extends _Users {
+
+    private static final long serialVersionUID = 1L; 
+
+}
