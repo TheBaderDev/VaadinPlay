813bb11706c27ce78493bb1742e9ea024e312d34
Klajd Deda <kdeda@id-design.com>
2019-05-20 11:44:43 -0400

    Fixes

--- vaadinplay/src/main/java/cayenne/Party.java
+++ vaadinplay/src/main/java/cayenne/Party.java
@@ -6,4 +6,9 @@ public class Party extends _Party {
 
     private static final long serialVersionUID = 1L;
 
+    // look up the user with type dj from the list of users on this party
+    //
+    public User getDJ() {
+        return null;
+    }
 }
