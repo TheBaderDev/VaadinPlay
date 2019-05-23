02beb3cc308b8acfe82bcdec536829f3c053cfde
Klajd Deda <kdeda@id-design.com>
2019-05-23 15:17:28 -0400

    Refactored

---
+++ vaadinplay/src/main/java/com/application/layouts/AdminView.java
@@ -0,0 +1,16 @@
+package com.application.layouts;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+/**
+ * Admin view that is registered dynamically on admin user login.
+ * <p>
+ * Allows CRUD operations for the book categories.
+ */
+public class AdminView extends VerticalLayout {
+    public static final String VIEW_NAME = "admin";
+
+    public AdminView() {
+    }
+
+}
