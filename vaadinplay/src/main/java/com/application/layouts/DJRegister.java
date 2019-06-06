a2c40aa24bbc7ebc707dba74fe4bb193e2772170
Klajd Deda <kdeda@id-design.com>
2019-06-06 15:43:10 -0400

    Fixes

--- vaadinplay/src/main/java/com/application/layouts/DJRegister.java
+++ vaadinplay/src/main/java/com/application/layouts/DJRegister.java
@@ -14,8 +14,9 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
 
-//@Route("djregister")
+@Route("djregister")
 @PageTitle("DJ Registration")
 @HtmlImport("MainBoxLayoutStyle.html")
 public class DJRegister extends VerticalLayout {
